79de2d3e34197ba2ae8d6fd87196af46e2633b6f
Mads Østergaard <m@madsostergaard.dk>
2018-06-07 12:05:17 +0200

    Added the trigger-example from https://developer.estimote.com/nearables/build-a-nearables-app/

--- MediCueApp/ViewController.swift
+++ MediCueApp/ViewController.swift
@@ -8,16 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ESTTriggerManagerDelegate {
 
+    let triggerManager = ESTTriggerManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.triggerManager.delegate = self
+        
+        /*
+            The following creates two rules ands appends them to a trigger.
+            When the
+        */
+        // create rules: (DON'T RUN - DOESN'T WORK)
+        let rule1 = ESTOrientationRule.orientationEquals(
+            .horizontalUpsideDown, for: .bag)
+        
+        let rule2 = ESTMotionRule.motionStateEquals(
+            true, forNearableIdentifier: "54b128ebbc420d35")
+        
+        // add rules to a trigger!
+        let trigger = ESTTrigger(rules: [rule1, rule2], identifier: "tom the trigger")
+        
+        self.triggerManager.startMonitoring(for: trigger)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func triggerManager(_ manager: ESTTriggerManager,
+                        triggerChangedState trigger: ESTTrigger) {
+        if (trigger.identifier == "tom the trigger" && trigger.state == true) {
+            print("Hello, digital world! The physical world has spoken.")
+        } else {
+            print("Goodnight. <spoken in the voice of a turret from Portal>")
+        }
     }
 
 
