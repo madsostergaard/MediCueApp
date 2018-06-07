9f6fe355586ba6b522e48cea51826fb65b16564f
Mads Østergaard <m@madsostergaard.dk>
2018-06-07 11:17:45 +0200

    Initial Commit

---
+++ MediCueApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediCueApp
+//
+//  Created by Mads Østergaard on 07/06/2018.
+//  Copyright © 2018 Mads Østergaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
