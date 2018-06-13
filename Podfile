e32a6c29bbce99de8e3741fb0d68980321a8a39f
Mads Østergaard <m@madsostergaard.dk>
2018-06-13 09:20:39 +0200

    added calendar kit

--- Podfile
+++ Podfile
@@ -9,6 +9,7 @@ target 'MediCueApp' do
   pod 'EstimoteSDK'
   pod 'Firebase/Core'
   pod 'Firebase/Database'
+  pod 'CalendarKit'
 
   target 'MediCueAppTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-MediCueApp/Pods-MediCueApp-frameworks.sh
+++ Pods/Target Support Files/Pods-MediCueApp/Pods-MediCueApp-frameworks.sh
@@ -143,12 +143,18 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CalendarKit/CalendarKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DateToolsSwift/DateToolsSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Neon/Neon.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CalendarKit/CalendarKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DateToolsSwift/DateToolsSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Neon/Neon.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
